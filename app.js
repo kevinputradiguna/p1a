5f3de3a276bc8bb57e296b4475bfe9e4ac054a07
kevinputradiguna <35824121+kevinputradiguna@users.noreply.github.com>
2018-01-26 09:53:23 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+var app = angular.module('bluebox',['ngRoute','ngTable', 'angularjs-datetime-picker', 'ngSanitize', 'ngResource']);
+
+app.service('detailServices', function(){
+    var historyDetails = "";
+    var evaluationDetails = "";
+    
+    this.setHistoryDetail = function(historyDetail){
+        historyDetails = historyDetail;
+    };
+
+    this.setEvaluationDetail = function(evaluationDetail){
+        evaluationDetails = evaluationDetail;
+    };
+    this.getHistoryDetail = function(){
+        return historyDetails;
+    };
+
+    this.getEvaluationDetail = function(){
+        return evaluationDetails;
+    };
+});
+
+app.service('locationService',function(){
+    var subLocation = "";
+
+    this.setSubLocation = function(location){
+        subLocation = location;
+    };
+
+    this.getSubLocation = function(){
+        return subLocation;
+    }
+});
+
+app.service('historyMemberService', function(){
+    var memberList = "";
+
+    this.setMemberList = function(member){
+        memberList = member;
+    }
+
+    this.getMemberList = function(){
+        return memberList;
+    }
+})
+
+app.service('userHeartBeatService', function(Rest){
+    this.doHeartBeat = function(){
+        var hdrs = {
+            "X-TOKEN": localStorage.getItem("edplusId") 
+        };
+        Rest.get('/v1/userHeartBeat', hdrs)
+        .success(function (result) {
+            console.log("detak jantung");
+        })
+        .error(function (error, status) {
+            console.log("error catch");
+        });
+    }
+})
+
+app.filter('type', function() {
+    return function(obj) {
+        return typeof obj;
+    }});
---
+++ app.routes.js
@@ -0,0 +1,61 @@
+app.config(function ($routeProvider) {
+	$routeProvider
+    
+		.when('/',{
+			template: "<signin></signin>"
+		})
+
+		.when('/archives',{
+			template: "<archives></archives>",
+			activetab: 'archives'
+		})
+
+		.when('/createroom',{
+			template: "<createroom></createroom>",
+			activetab: 'createroom'
+		})
+
+		.when('/conference',{
+			template: "<demo></demo>"
+		})
+
+		.when('/evaluation',{
+			template: "<evaluation></evaluation>",
+			activetab: 'evaluation'
+		})
+
+		.when('/evaluationdetail',{
+			template: "<evaluationdetail></evaluationdetail>",
+			activetab: 'evalution'
+		})
+
+		.when('/history',{
+			template: "<history></history>",
+			activetab: 'history'
+		})
+
+		.when('/historydetail',{
+			template: "<historydetail></historydetail>",
+			activetab: 'history'
+		})
+
+		.when('/joinroom',{
+			template: "<joinroom></joinroom>",
+			activetab: 'joinRoom'
+		})
+
+		.when('/profile',{
+			template: "<profile></profile>",
+			activetab: 'profile'
+		})
+
+		.when('/statistic',{
+			template: "<statistic></statistic>",
+			activetab: 'statistic'
+		})
+		
+		.otherwise({
+			redirectTo: '/'
+		})
+
+});
